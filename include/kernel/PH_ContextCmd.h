d36a679b4b76fcda688724d91b0d617b0f24ba8a
Bruno Sanches <bcsanches@gmail.com>
2010-03-06 01:41:13 -0300

    + First revision for context

---
+++ include/kernel/PH_ContextCmd.h
@@ -0,0 +1,89 @@
+/*
+Phobos 3d
+  March 2010
+
+  Copyright (C) 2005-2010 Bruno Crivelari Sanches
+
+  This software is provided 'as-is', without any express or implied
+  warranty. In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+
+  Bruno Crivelari Sanches bcsanches@gmail.com
+*/
+
+#ifndef PH_CONTEXT_CMD_H
+#define PH_CONTEXT_CMD_H
+
+#include "PH_ContextItem.h"
+#include "PH_Defs.h"
+
+#include <boost/function.hpp>
+#include <vector>
+
+namespace Phobos
+{
+	typedef std::vector<String_c> StringVector_t;
+	typedef boost::function1<void, const StringVector_t &> CmdProc_t;
+
+	class PH_KERNEL_API ContextCmd_c: public ContextItem_c
+	{	
+		public:
+			// =====================================================
+			// PUBLIC METHODS
+			// =====================================================
+			inline ContextCmd_c(const String_c &name, CmdProc_t proc = CmdProc_t());		
+			inline ContextCmd_c(const ContextCmd_c &other);
+
+			inline void Execute(const StringVector_t &args);
+
+			inline void SetProc(CmdProc_t proc);
+
+		protected:
+			// =====================================================
+			// PROTECTED ATTRIBUTES
+			// =====================================================
+			CmdProc_t		pfnProc;			
+	};
+
+	// =====================================================
+	// INLINE IMPLEMENTATION
+	// =====================================================
+
+	inline ContextCmd_c::ContextCmd_c(const String_c &name, CmdProc_t proc):
+		ContextItem_c(name),
+		pfnProc(proc)
+	{
+		//empty
+	}
+
+	inline ContextCmd_c::ContextCmd_c(const ContextCmd_c &other):
+		ContextItem_c(other),
+		pfnProc(other.pfnProc)
+	{
+		//empty
+	}
+
+	inline void ContextCmd_c::SetProc(CmdProc_t proc)
+	{
+		pfnProc = proc;
+	}
+
+	inline void ContextCmd_c::Execute(const StringVector_t &args)
+	{	
+		pfnProc(args);
+	}
+}
+
+#endif
