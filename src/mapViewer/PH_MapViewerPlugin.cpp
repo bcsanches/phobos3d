adffe49a4256e5bdc9196b41d396406c32ee3fd3
Bruno Sanches <bcsanches@gmail.com>
2010-10-04 23:55:21 -0300

    + Added base project for creating a map viewer, updates issue 027

---
+++ src/mapViewer/PH_MapViewerPlugin.cpp
@@ -0,0 +1,67 @@
+/*
+Phobos 3d
+  October 2010
+
+  Copyright (C) 2005-2010 Bruno Crivelari Sanches
+
+  This software is provided 'as-is', without any express or implied
+  warranty. In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+
+  Bruno Crivelari Sanches bcsanches@gmail.com
+*/
+#include "PH_MapViewerPlugin.h"
+
+#include <PH_Core.h>
+#include <PH_Plugin.h>
+#include <PH_ProcVector.h>
+
+namespace Phobos
+{
+	class MapViewerPlugin_c: public IPluginInstance_c
+	{
+		public:
+			void Init();
+			void Finalize();
+
+		private:
+			ProcVector_c clProcs;
+	};
+
+	void MapViewerPlugin_c::Init()
+	{
+		CorePtr_t core = Core_c::GetInstance();
+
+		/*
+		WorldManagerPtr_t worldManager = WorldManager_c::CreateInstance();
+		clProcs.AddProc(WorldManager_c::ReleaseInstance);
+		core->AddModule(worldManager);		*/
+	}
+
+	void MapViewerPlugin_c::Finalize()
+	{
+		clProcs.CallAll();
+		clProcs.Clear();
+	}
+}
+
+Phobos::IPluginInstance_c *PH_PluginEntryPoint(void)
+{
+	using namespace Phobos;
+
+	static MapViewerPlugin_c plugin;
+
+	return &plugin;
+}
