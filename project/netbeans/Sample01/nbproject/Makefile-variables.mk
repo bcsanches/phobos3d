ee2eab73653129201df9bcb8f233203bcad1217b
bcsanches <bcsanches@gmail.com>
2011-09-01 12:08:36 -0300

    Netbeans project

---
+++ project/netbeans/Sample01/nbproject/Makefile-variables.mk
@@ -0,0 +1,24 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Debug=sample01
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/sample01
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=sample01.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/sample01.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=sample01
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/sample01
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=sample01.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/sample01.tar
