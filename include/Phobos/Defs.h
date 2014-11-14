941ecc011bc8786228d3bb9ae9a3648779177690
Francisco Neto <cronosts@gmail.com>
2014-11-13 21:10:15 -0300

    Removed PH_SDL or PH_NATIVE checking.

--- include/Phobos/Defs.h
+++ include/Phobos/Defs.h
@@ -58,10 +58,6 @@ subject to the following restrictions:
 	#error "Phobos: please define PH_MSVC or PH_GCC"
 	#endif
 
-	#if ((!defined PH_NATIVE) && (!defined PH_SDL))
-	#error "Phobos: please define PH_SDL or PH_NATIVE"
-	#endif
-
 	#if (defined PH_WIN32)
 
 		#if(defined NDEBUG)
